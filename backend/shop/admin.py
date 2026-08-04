cf9b82b3df672a2b1025f53d02ff3b4a13e13e3c
olena.kostryk <240208612@ostimteknik.edu.tr>
2026-08-04 12:27:12 +0300

    fix some bug

--- backend/shop/admin.py
+++ backend/shop/admin.py
@@ -1,60 +1,7 @@
 from django.contrib import admin
 from .models import Flower, Order, OrderItem
 
-from django.contrib import admin
-from .models import Flower, Order, OrderItem
-
-
-class OrderItemInline(admin.TabularInline):
-    model = OrderItem
-    extra = 0
-
-
-@admin.register(Order)
-class OrderAdmin(admin.ModelAdmin):
-    list_display = (
-        "id",
-        "first_name",
-        "last_name",
-        "email",
-        "total_price",
-        "status",
-        "created_at",
-    )
-
-    list_filter = (
-        "status",
-        "created_at",
-    )
-
-    search_fields = (
-        "first_name",
-        "last_name",
-        "email",
-    )
-
-    inlines = [OrderItemInline]
-
-
-@admin.register(Flower)
-class FlowerAdmin(admin.ModelAdmin):
-    list_display = (
-        "name",
-        "category",
-        "price",
-        "stock",
-        "featured",
-    )
-
-    list_filter = (
-        "category",
-        "featured",
-    )
-
-    search_fields = ("name",)
-
-
-admin.site.register(OrderItem)
+admin.site.register(Order)
 admin.site.register(OrderItem)
 
 @admin.register(Flower)
