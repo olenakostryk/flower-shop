4fefa0d0b01e7eb1311fb0081151f6855850dec4
olena.kostryk <240208612@ostimteknik.edu.tr>
2026-08-04 12:48:14 +0300

    Prepare backend for Render deployment

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -3,6 +3,8 @@ Django settings for config project.
 """
 
 from pathlib import Path
+
+import dj_database_url
 from decouple import config
 
 # --------------------------------------------------
@@ -94,10 +96,10 @@ WSGI_APPLICATION = "config.wsgi.application"
 # --------------------------------------------------
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+    )
 }
 
 # --------------------------------------------------
